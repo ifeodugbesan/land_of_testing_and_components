0755e1a900e76b0dfb5e067d23b3f6f9dad6e1ff
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-06-04 19:43:28 +0100

    created loop beauty page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :beauty_loop, :holding_page, :send_email]
 
   def home
   end
+
+  def beauty_loop
+  end
+
+  def holding_page
+  end
+
+  def send_email
+    # message = params[:send_email]
+    # ContactMailer.send_email_to_contact(message).deliver_now
+    # redirect_to storages_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/beauty_loop', to: 'pages#beauty_loop'
+  get '/holding_page', to: 'pages#holding_page'
+  get "/send_email", to: 'pages#send_email'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
